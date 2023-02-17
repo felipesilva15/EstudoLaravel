c441d236f0f0bd8604ddcc5e5e4a856953e62057
felipe.silva <felipe.silva@pwi.com.br>
2023-02-16 22:54:59 -0300

    Aprendendo sobre layouts e arquivos estáticos públicos do Blade

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>@yield('title')</title>
+
+        <!-- Fonte do Google -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto" rel="stylesheet">
+
+        <!-- CSS do bootstrap -->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+
+        <!-- CSS da aplicação -->
+        <link rel="stylesheet" href="/css/style.css">
+    </head>
+    <body>
+        <main>
+            @yield('content')
+        </main>
+        <footer>
+            <p style="text-align: center; font-weight: bold;">Felipe's Events &copy; 2023</p>
+        </footer>
+
+        <!-- JS do bootstrap -->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
+        <!-- JS da aplicação -->
+        <script src="/js/script.js"></script>
+    </body>
+</html>
--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,13 +1,7 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
+@extends('layouts.main')
+
+@section('title', 'Produtos')
+@section('content')
     <h1>Tela de produtos</h1>
     <a href="/">Voltar para a home</a>
-</body>
-</html>
+@endsection
