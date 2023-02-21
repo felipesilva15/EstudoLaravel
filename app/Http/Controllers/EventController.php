2e5b3e1fb1b59462b8f80cb8d3aed6be08104b07
Felipe Silva <felipe.allware@gmail.com>
2023-02-21 01:24:48 -0300

    Atualizando dashboard e criando funcionalidade para deletar eventos

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -63,4 +63,18 @@ class EventController extends Controller
 
         return view('events.show', ['event' => $event, 'eventOwner'=>$eventOwner]);
     }
+
+    public function dashboard(){
+        $user = auth()->user();
+
+        $events = $user->events;
+
+        return view('events.dashboard', ['events'=>$events]);
+    }
+
+    public function destroy($id){
+        Event::findOrFail($id)->delete();
+
+        return redirect('/dashboard')->with('msg', 'Evento deletado com sucesso!');
+    }
 }
---
+++ resources/views/events/dashboard.blade.php
@@ -0,0 +1,41 @@
+@extends('layouts.main')
+
+@section('title', 'Dashboard')
+@section('content')
+    <div class="col-md-10 offset-md-1 dashboard-title-container my-4">
+        <h1>Meus eventos</h1>
+    </div>
+    <div class="col-md-10 offset-md-1 dashboard-events-container">
+        @if (count($events) > 0)
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">#</th>
+                        <th scope="col">Nome</th>
+                        <th scope="col">Participantes</th>
+                        <th scope="col">Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($events as $event)
+                        <tr>
+                            <th scropt="row">{{ $loop->index + 1 }}</th>
+                            <td><a href="/events/{{ $event->id }}">{{ $event->title }}</a></td>
+                            <td>0</td>
+                            <td>
+                                <a href="#" class="btn btn-info"><span class="material-icons">edit_square</span> Editar</a>
+                                <form action="/events/{{ $event->id }}" method="POST">
+                                    @csrf
+                                    @method('DELETE')
+                                    <button type="submit" class="btn btn-danger"><span class="material-icons">delete</span> Excluir</button>
+                                </form>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        @else
+            <p>Você ainda não tem eventos, <a href="/events/create">criar evento.</a></p>
+        @endif
+    </div>
+@endsection
